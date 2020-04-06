173c95fec73e96f988ec0674f5aad50fc9ae8904
pmusgrave <ps.musgrave@gmail.com>
2020-04-06 15:18:22 -0400

    feat: implement basic redux store structure
    This borrows from the chirper example project to create the basic
    project structure with actions, components, middleware, reducers,
    and utils directories as the source tree. The main app component gets
    initial data from the fake server, and dispatches actions for users and
    questions. For now, it just logs the redux store to the console upon
    updating the users and questions state.

---
+++ src/actions/shared.js
@@ -0,0 +1,32 @@
+import { _getUsers, _getQuestions } from '../utils/_DATA';
+import { receiveUsers } from '../actions/users';
+import { receiveQuestions } from '../actions/questions';
+import { setAuthedUser } from '../actions/authedUser';
+import { showLoading, hideLoading } from 'react-redux-loading'
+
+export function handleInitialData() {
+	return (dispatch) => {
+		dispatch(showLoading());
+
+		return Promise.all([
+	      _getUsers(),
+	      _getQuestions()])
+	    .then(([users, questions]) => ({
+			  users,
+			  questions,
+			}))
+	    .then((data) => {
+	    	dispatch(receiveUsers(data.users));
+	    	dispatch(receiveQuestions(data.questions));
+	    	dispatch(hideLoading());
+		});
+
+		// return getInitialData()
+		// .then(({ users, tweets }) => {
+		// 	dispatch(receiveUsers(users));
+		// 	dispatch(receiveTweets(tweets));
+		// 	dispatch(setAuthedUser(AUTHED_ID));
+		// 	dispatch(hideLoading());
+		// })
+	}
+}
---
+++ src/components/App.js
@@ -0,0 +1,50 @@
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import LoadingBar from 'react-redux-loading'
+import logo from '../resources/logo.svg';
+import './App.css';
+import { handleInitialData } from '../actions/shared'
+// import { _getUsers, _getQuestions } from '../utils/_DATA'
+
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+
+  render(){
+    return (
+      <Router>
+        <Fragment>
+          <LoadingBar/>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          </div>
+        </Fragment>
+      </Router>
+    );
+  }
+}
+
+function mapStateToProps ({ authedUser, users, questions }) {
+  return {
+    loading: authedUser === null,
+    users,
+    questions,
+  }
+}
+
+export default connect(mapStateToProps)(App)
